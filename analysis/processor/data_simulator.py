8bae8b9b0a7dabbf0c8f609e13a89568177e262d
Ebad Kamil <ebad.kamil@xfel.eu>
2020-08-10 11:38:46 +0200

    Add DataSimulator that feeds data in a process.

---
+++ analysis/processor/data_simulator.py
@@ -0,0 +1,65 @@
+"""
+Analysis and visualization software
+
+Author: Ebad Kamil <ebad.kamil@xfel.eu>
+All rights reserved.
+"""
+
+from datetime import datetime
+import time
+import multiprocessing as mp
+import queue
+
+import numpy as np
+
+
+class DataSimulator(mp.Process):
+    def __init__(self, sim_queue, data_shape=None):
+        super().__init__()
+
+        self._sim_queue = sim_queue
+        self._data_shape = data_shape
+        self._running = False
+
+    def run(self):
+        self._running = True
+
+        while self._running:
+            shape = self._data_shape \
+                if self._data_shape is not None else (10, 1024, 1024)
+
+            # Random image data emulating rings
+            background =  2. * np.random.rand(*shape)
+            x = np.linspace(-1, 1, shape[-2])
+            y = np.linspace(-1, 1, shape[-1])
+            xx, yy = np.meshgrid(x, y)
+            z = 10. * np.sin(
+                np.random.randint(1, 20) * np.pi * (xx**2 + yy**2))
+
+            data = {'image': background + z}
+            meta = {'timestamp': datetime.now().strftime(
+                    "%d-%b-%Y (%H:%M:%S.%f)")}
+
+            payload = (meta, data)
+            try:
+                self._sim_queue.put_nowait(payload)
+                time.sleep(.1)
+            except queue.Full:
+                continue
+
+    def terminate(self):
+        self._running = False
+
+
+if __name__ == "__main__":
+    sim_queue = mp.Queue(maxsize=1)
+    simulator = DataSimulator(sim_queue)
+    simulator.daemon = True
+    simulator.start()
+    while True:
+        try:
+            meta, data = sim_queue.get_nowait()
+            print(meta)
+        except queue.Empty:
+            continue
+    simulator.terminate()
