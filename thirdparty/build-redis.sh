7bedbfec8897a242f35c219e1c7d81cf718c99eb
Ebad Kamil <ebad.kamil@xfel.eu>
2020-08-10 11:36:59 +0200

    Add redis building script

---
+++ thirdparty/build-redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+TP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"/../
+ROOT_DIR=${TP_DIR}/..
+
+REDIS_VERSION=5.0.4
+REDIS_BUILD_DIR=${TP_DIR}/pkg/redis/
+
+if [[ ! -f ${REDIS_BUILD_DIR}/src/redis-server ]]; then
+
+    REDIS_DOWNLOAD=redis-stable.tar.gz
+    wget http://download.redis.io/${REDIS_DOWNLOAD} -P ${TP_DIR}
+
+    mkdir -p ${REDIS_BUILD_DIR}
+    tar -xzf ${TP_DIR}/${REDIS_DOWNLOAD} -C ${REDIS_BUILD_DIR}
+    rm ${TP_DIR}/${REDIS_DOWNLOAD}
+
+    pushd ${REDIS_BUILD_DIR}
+        make
+    popd
+fi
+
+mkdir -p ${ROOT_DIR}/extra-analysis/thirdparty/bin
+cp ${REDIS_BUILD_DIR}/src/redis-server ${ROOT_DIR}/extra-analysis/thirdparty/bin/redis-server
+cp ${REDIS_BUILD_DIR}/src/redis-cli ${ROOT_DIR}/extra-analysis/thirdparty/bin/redis-cli
